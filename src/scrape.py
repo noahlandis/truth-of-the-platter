71fd9b54f50c4ec7eab029a000a63d175a84b118
Noah Landis <noahlandis980@gmail.com>
2024-05-17 12:38:09 -0500

    refactored and replaced get_fuzzy_matches with is_fuzzy_match

--- src/scrape.py
+++ src/scrape.py
@@ -9,12 +9,12 @@ import re
 # 2. Create URL builder (interface/higher order functions?)
 # 3. Implement Regex Matching feature
 # 4. Create front end
-
+YELP_ROOT = "https://www.yelp.com"
 
 
 def read_input():
     name = input("Enter a restaraunt name: ")
-    location = input("Enter the restaraunt's location: ")
+    location = input("Enter the name of a city: ")
     scrape(name, location)
 
 def get_html(name, location):
@@ -25,47 +25,65 @@ def get_html(name, location):
     return soup
 
 def url_builder_yelp(name, location):
-    return f"https://www.yelp.com/search?find_desc={name}&find_loc={location}"
+    return f"{YELP_ROOT}/search?find_desc={name}&find_loc={location}"
 
 def scrape(name, location):
-    soup = get_html(name, location)
-    buisness_name_tags = soup.select('[class*="businessName"]', limit=10)
+    soup = get_html(name, location)    
+    
+    # collect buisness name tags
+    filtered_tags = get_filtered_tags(name, soup)
+    pages, search_results = get_pages_and_search_results(filtered_tags)
+    
 
-    # get the text from the buisness name tags
-    potential_intended_restaurant_names = []
+    print(search_results)
+    
+def get_filtered_tags(name, soup):
+    buisness_name_tags = soup.select('[class*="businessName"]', limit=10)
+    filtered_tags = []
+    # only collect tags that match name
     for tag in buisness_name_tags:
         buisness_name = tag.get_text(strip=True)
-
         # As sponsored results are irrelevant to the search, we ignore them. Only non-sponsored results are in the format "integer.<name>. 
-        if re.match(r'^\d\.', buisness_name):
-            potential_intended_restaurant_names.append(buisness_name)
+        if re.match(r'^\d\.', buisness_name) and is_fuzzy_match(name, buisness_name):
+            # remove integer and period as it isn't relevant
+            buisness_name = buisness_name[2::]
 
-    # in case of multiple sponsored results, we only consider numbered results
-    filtered_matches = get_fuzzy_matches(name, potential_intended_restaurant_names)
-    print(filtered_matches)
+            # we store the buisness name as well so we don't need another list
+            filtered_tags.append((tag, buisness_name))
+    return filtered_tags
+
+
+def get_pages_and_search_results(filtered_tags):
+
+    # get the individual page url's for the filtered tags
+    pages = []
+
+    # aggregate in form (buisness name, location)
+    search_results = []
     
-    
-
-
-
+    # we collect address info here. We don't collect ratings, because it's only needed for the restaraunt the user selects
+    for filtered_tag, buisness_name in filtered_tags:
+        restaraunt_url = filtered_tag.find('a', href=True)['href']
+        url = f"{YELP_ROOT}" + restaraunt_url
+        individual_restaraunt_page = requests.get(url, headers={'User-Agent': "Mozilla/5.0"})
+        page = BeautifulSoup(individual_restaraunt_page.text, 'html.parser')
+        # we only need to get the rating for the one the user clicks, so we store every web page just in case
+        pages.append(page)
+        element = page.find(string="Get Directions")
+        location = element.parent.parent.next_sibling.string
+        search_results.append((buisness_name, location))
+    return pages, search_results
 
 def remove_non_alphanumeric_chars(a_string):
     return re.sub(r'\W+', '', a_string)
 
-def get_fuzzy_matches(query, choices):
-    query = remove_non_alphanumeric_chars(query)
-    choices_copy = [remove_non_alphanumeric_chars(element) for element in choices]
-    matches = process.extract(query, choices_copy, scorer=fuzz.partial_token_set_ratio) 
-    # only extract matches with ratio above 75
-    filtered_matches = []
-    for i in range(len(matches)):
-        # choices copy is what we used for filtering, we want to add the names of the original restaraunts when we match to the HTML parsing
-        name = choices[i]
-        ratio = matches[i][1]
-        if ratio > 75:
-            filtered_matches.append(name)
+def is_fuzzy_match(user_input, search_result):
+    user_input = remove_non_alphanumeric_chars(user_input)
+    search_result = remove_non_alphanumeric_chars(search_result)
+    match_ratio = fuzz.partial_token_set_ratio(user_input, search_result)
+    return match_ratio > 70
 
-    return filtered_matches
+
 
 
 
@@ -79,6 +97,7 @@ def main():
     # string in first search box: luigis
     # string in second search box: fairfield
 
-    url = "https://www.yelp.com/search?find_desc=luigis&find_loc=fairfield"
-    scrape("homeslice", "austin")
-main()
+    read_input()
+
+if __name__ == "__main__":
+    main()
--- test/test_scrape.py
+++ test/test_scrape.py
@@ -10,7 +10,7 @@ def test_url_builder_yelp():
     # analyze
     assert expected == actual
 
-def test_remove_whitespace():
+def test_remove_non_alphanumeric_chars():
     # setup
     a_string = "\tHomeslice's Pizza, Restaraunt"
     expected = "HomeslicesPizzaRestaraunt"
@@ -19,24 +19,61 @@ def test_remove_whitespace():
     # analyze
     assert expected == actual
 
-def test_fuzzy_matching_all_strings_match():
+def test_is_fuzzy_match_true_different_capitalization():
     # setup
-    query = "homeslice pizza"
-    choices = ["Homeslice Pizza", "homeslice's Pizzeria", "homeslice", "Pizza Homeslice"]
+    user_input = "home slice pizza"
+    search_result = "Home Slice Pizza"
     # invoke
-    actual = src.scrape.get_fuzzy_matches(query, choices)
-    # analyze
-    assert choices == actual
-    
-def test_fuzzy_matching_no_strings_match():
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
+    # analyze 
+    assert actual == True
+
+def test_is_fuzzy_match_true_different_spacing():
     # setup
-    query = "homeslice pizza"
-    choices = ["Andrew's home kitchen", "Pizza palace", "Subway", "luigis Pizzeria"]
-    expected = []
+    user_input = "HomeSlicePizza"
+    search_result = "Home Slice Pizza"
     # invoke
-    actual = src.scrape.get_fuzzy_matches(query, choices)
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
     # analyze
-    assert expected == actual
+    assert actual == True
+
+def test_is_fuzzy_match_true_shortened_name():
+    # setup
+    user_input = "Home Slice"
+    search_result = "Home Slice Pizza"
+    # invoke
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
+    # analyze
+    assert actual == True
+
+
+def test_is_fuzzy_match_true_different_word():
+    # setup
+    user_input = "Home Slice Restaraunt"
+    search_result = "Home Slice Pizza"
+    # invoke
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
+    # analyze
+    assert actual == True
+
+def test_is_fuzzy_match_true_different_punctuation():
+    # setup
+    user_input = "Home Slice's Pizza"
+    search_result = "Home Slice Pizza"
+    # invoke
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
+    # analyze
+    assert actual == True
+
+
+def test_is_fuzzy_match_false():
+    # setup
+    user_input = "Luigi's Pizza from Home"
+    search_result = "Home Slice Pizza"
+    # invoke
+    actual = src.scrape.is_fuzzy_match(user_input, search_result)
+    # analyze
+    assert actual == False
 
 
     
