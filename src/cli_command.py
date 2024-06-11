b0efaef06c48d6c04ae493954a5b98f5eabbfc88
Noah Landis <noahlandis980@gmail.com>
2024-06-11 14:30:26 -0400

    used constants to define commands

--- src/cli_command.py
+++ src/cli_command.py
@@ -2,16 +2,22 @@
 This module contains the functions to handle user commands
 Author: Noah Landis
 """
-
+import types
 import os
 import sys
 from colorama import Style
 from utils.styled_cli_utils import MessageType, get_styled_input, get_styled_output
 
+# not the cleanest, but we use const to avoid "Irrefutable pattern is allowed only for the last case statementPylance error in the switch statement
+const = types.SimpleNamespace()
+const.HELP = "\\h"
+const.RESTART = "\\r"
+const.QUIT = "\\q"
+
 COMMANDS = {
-    "\\h": "Display this help menu",
-    "\\r": "Restart the program",
-    "\\q": "Quit the program"
+    const.HELP: "Display this help menu",
+    const.RESTART: "Restart the program",
+    const.QUIT: "Quit the program"
 }
 
 def display_commands():
@@ -43,14 +49,14 @@ def handle_commands(user_input: str) -> bool:
     :return bool - True if the user input is a command, False otherwise
     """
     match user_input:
-        case "\\h":
+        case const.HELP:
             display_commands()
             return True
-        case "\\r":
+        case const.RESTART:
             # we want a new line to make it clear the program is restarting
             print()
             os.execl(sys.executable, sys.executable, *sys.argv)
-        case "\\q":
+        case const.QUIT:
             sys.exit()
     return False
 
@@ -61,5 +67,5 @@ def prompt_next_command():
     user_input = ""
     # prompt the user until a valid command is entered
     while user_input not in COMMANDS.keys():
-        user_input = get_styled_input("Enter '\\r' to search again, or '\\q' to quit the program")
+        user_input = get_styled_input(f"Enter '{const.RESTART}' to search again, or '{const.QUIT}' to quit the program")
     handle_commands(user_input)
